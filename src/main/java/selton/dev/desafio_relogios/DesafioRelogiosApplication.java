44b6e931b5ce4506392a366255eb555f4d1651a5
Selton Augusto <selthive@gmail.com>
2026-02-12 16:11:36 -0300

    first commit: create basic project and db config

---
+++ src/main/java/selton/dev/desafio_relogios/DesafioRelogiosApplication.java
@@ -0,0 +1,13 @@
+package selton.dev.desafio_relogios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRelogiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRelogiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/selton/dev/desafio_relogios/DesafioRelogiosApplicationTests.java
@@ -0,0 +1,13 @@
+package selton.dev.desafio_relogios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRelogiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
